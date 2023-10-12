6b5001cb1de1b35558437ad13b092675ad602da3
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-10-12 18:10:59 -0400

    light/dark mode added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -30,8 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+          storageKey='jotter-theme'
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
